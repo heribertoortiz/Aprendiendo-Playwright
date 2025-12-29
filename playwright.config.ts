2abf9a4d80ecd9359c6ffcaf7198e4ce42f81756
Heriberto <heriberto9705@gmail.com>
2025-12-29 15:34:56 -0600

    Se separan los spec y se agregan configuraciones

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,9 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  reporter: 'html',
+  fullyParallel: true,
+  workers: 3,
   webServer: {
     command: 'npm start', //Indicamos que se inicie automaticamente el servidor web a traves de la url local
     url: 'http://localhost:5000/',
@@ -10,5 +13,27 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:5000/', //Al iniciar la ejecución de la prueba automaticamente se redirecciona a la url especificada
     headless: false
-  }
+    // locale: 'en-GB'
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], },
+      dependencies:[
+        'auth-setup'
+      ]
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+      dependencies:[
+        'auth-setup'
+      ]
+    },
+    {
+      name: 'auth-setup',
+      testMatch: 'tests/setup/Auth.setup.ts',
+    }
+  ]
 });
